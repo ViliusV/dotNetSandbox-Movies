0d6768b9cabbf459a06e22874144d10f4724e538
vilius.vaicekauskas <vilius.vaicekauskas@outlook.com>
2016-12-15 18:34:56 +0200

    Adding Youtube API to get trailers

--- Movies/src/Movies.Services/Services/YouTubeService.cs
+++ Movies/src/Movies.Services/Services/YouTubeService.cs
@@ -1,11 +1,26 @@
-﻿
+﻿using Google.Apis.Services;
+using System.Linq;
+using YouTubeApiService = Google.Apis.YouTube.v3.YouTubeService;
+
 namespace Movies.Services.Services
 {
     public class YouTubeService : IVideoProviderService
     {
+
+
         public string GetTrailerVideoUrl(string movieTitle)
         {
-            return "https://www.youtube.com/embed/3ycmmJ6rxA8"; //ToDo: Get from Api
+            //ToDo: store api key in config
+            var service = new YouTubeApiService(new BaseClientService.Initializer() { ApiKey = "AIzaSyBCKvubbI7mGVNNjHV_sPpk_sho-Jsa_JQ" });
+            var searchRequest = service.Search.List("snippet");
+            searchRequest.Q = $"{movieTitle} trailer";
+            searchRequest.MaxResults = 1;
+
+            var searchResponse = searchRequest.Execute(); //ToDo: use await
+
+            var videoUrl = searchResponse.Items?.FirstOrDefault()?.Id?.VideoId;
+
+            return videoUrl;
         }
     }
 }
