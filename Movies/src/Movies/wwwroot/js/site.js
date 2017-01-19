010600716d8df00a2081ec96a7e91e0709124d35
vilius.vaicekauskas <vilius.vaicekauskas@outlook.com>
2017-01-19 18:25:42 +0200

    Javascript refactoring

--- Movies/src/Movies/wwwroot/js/site.js
+++ Movies/src/Movies/wwwroot/js/site.js
@@ -2,29 +2,41 @@
 
 //ToDo: Bundle files
 $(function () {
-    $('.trailer-load-button').click(function () {
-        var button = $(this);
-        var movie = $(button).closest('.movie');
-        var title = movie.data('title');
-        
-        $.ajax({
-            url: 'api/MoviesApi/GetTrailerVideoId',
-            data: {
-                title: title
-            },
-            success: function(data){
-                if (typeof data !== 'undefined' && data !== null && data !== '') {
-                    var trailerVideo = movie.find('.trailer-video');
-                    var trailerVideoIframe = movie.find('.trailer-video iframe');
-                    var source = $(trailerVideoIframe).prop('src') + data;
-
-                    $(trailerVideoIframe).prop('src', source);
-                    $(trailerVideo).show();
-                    $(button).hide();
-                }
-            }
-
-            //ToDo: on error show error message, don't allow to click button once more
-        });
-    });
+	$('.trailer-load-button').click(function () {
+		loadButtonClick($(this));
+	});
 });
+
+var loadButtonClick = function (button) {
+	var movie = $(button).closest('.movie');
+	var title = movie.data('title');
+        
+	$.ajax({
+		url: 'api/MoviesApi/GetTrailerVideoId',
+		data: {
+			title: title
+		},
+		success: function(data){
+			if (typeof data !== 'undefined' && data !== null && data !== '') {
+				appendVideo(movie, data);
+				$(button).hide();
+			}
+		}
+
+		//ToDo: on error show error message, don't allow to click button once more
+	});
+}
+
+var appendVideo = function (movieElement, id) {
+	var movieElementExists = typeof movieElement !== "undefined" && movieElement !== null;
+	var idExists = typeof id !== "undefined" && id !== null && id !== "";
+
+	if (movieElementExists && idExists) {
+		var trailerVideo = movieElement.find('.trailer-video');
+		var trailerVideoIframe = movieElement.find('.trailer-video iframe');
+		var source = $(trailerVideoIframe).prop('src') + id;
+
+		$(trailerVideoIframe).prop('src', source);
+		$(trailerVideo).show();
+	}
+}
